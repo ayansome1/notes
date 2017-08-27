1fa9ad467e2189ce3508e9be1df2f1565bd59f2c
ayan <ayansome1@gmail.com>
2017-08-27 20:39:55 +0530

    bodyparser use and cookie parser remove

--- server/index.js
+++ server/index.js
@@ -9,14 +9,16 @@ let http = require('http');
 let passport = require('passport');
 let app = express();
 let session = require('express-session');
+
+//https://medium.com/@adamzerner/how-bodyparser-works-247897a93b90
+//Using body parser allows you to access req.body from within your routes
 let bodyParser = require('body-parser');
-let cookieParser = require('cookie-parser');
+
 let MySQLStore = require('express-mysql-session')(session);
 const pool = require('./lib/mysql-db-pool');
 
 let sessionStore = new MySQLStore({},pool);
 
-app.use(cookieParser());
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
