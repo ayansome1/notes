0d1e84d90ef70cebb37645f889e792888dda645d
ayan <ayansome1@gmail.com>
2017-11-05 20:17:00 +0530

    client and server set up

--- server/lib/mysql-db-pool.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* global require,console*/
-/* jshint node: true*/
-
-var mysql = require('mysql');
-
-let config = '../config/config.json';
-let pool = mysql.createPool(config.sqlconn);
-module.exports = pool;
