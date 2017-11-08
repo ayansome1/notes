d6e322610a1bcce1cd98ab50c3b095c8cd44967c
ayan <ayansome1@gmail.com>
2017-11-08 14:00:32 +0530

    add note modal

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -2,18 +2,43 @@
 /*jshint node:true, quotmark:false*/
 /*global angular,alert,_,moment*/
 angular.module('notesApp')
-	.controller('homeController', ['$scope', '$http', 'baseUrl', '$rootScope', '$state', '$uibModal', function ($scope, $http, baseUrl, $rootScope, $state, $uibModal) {
+	.controller('homeController', ['$scope', '$http', 'baseUrl', '$rootScope', '$state', '$uibModal', function($scope, $http, baseUrl, $rootScope, $state, $uibModal) {
 
 		function getNotes() {
 
-			$http.get(baseUrl + "/notes").then((response)=> {
-				console.log(response.data);
-				$scope.allNotes = response.data;
-			})
-			.catch((response)=> {
-				console.log(response);
-			});
+			$http.get(baseUrl + "/notes").then((response) => {
+					console.log(response.data);
+					$scope.allNotes = response.data;
+				})
+				.catch((response) => {
+					console.log(response);
+				});
 		}
+
+		let noteModal;
+		$scope.openNoteModal = function(isNew) {
+
+			if (isNew) {
+				$scope.addOrEdit = 'Add';
+
+			} else {
+				$scope.addOrEdit = 'Edit';
+			}
+
+			if (isNew) {
+				$scope.newNote = {};
+			}
+
+			noteModal = $uibModal.open({
+				templateUrl: 'components/home/edit-note.html',
+				scope: $scope,
+				size: 'md'
+			});
+		};
+
+		$scope.closeNoteModal = function() {
+			noteModal.close();
+		};
 
 		getNotes();
 
