25d76821abd47f0f1e6e486ecc18c91c6b0b5089
ayan <ayansome1@gmail.com>
2017-11-13 16:06:23 +0530

    prod chk

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,13 @@ var environment = process.env.NODE_ENV;
 console.log(environment);
 var config;
 if (environment === 'production') {
+	console.log("production");
 	config = require('./config_prod.json');
 } else if (environment === 'staging') {
+	console.log("staging");
 	config = require('./config_test.json');
 } else {
+	console.log("local");
 	config = require('./config.json');
 }
 
