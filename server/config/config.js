58ea2f81a52e53e0caaa3b6f2e540b8f458e0026
ayan <ayansome1@gmail.com>
2017-11-13 16:33:02 +0530

    test

--- server/config/config.js
+++ server/config/config.js
@@ -4,16 +4,13 @@
 /* global process, console, exports:true, module */
 
 var environment = process.env.NODE_ENV;
-console.log(environment);
+console.log("environmement is " ,environment);
 var config;
 if (environment === 'production') {
-	console.log("production");
 	config = require('./config_prod.json');
 } else if (environment === 'staging') {
-	console.log("staging");
 	config = require('./config_test.json');
 } else {
-	console.log("local");
 	config = require('./config.json');
 }
 
