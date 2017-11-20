2dbbfd071c83a024c8aa4b56852d9e1fb8077cd1
ayan <ayansome1@gmail.com>
2017-11-20 10:24:11 +0530

    user image and logout

--- client/app/app.main.js
+++ client/app/app.main.js
@@ -2,8 +2,18 @@
 /*jshint node:true, quotmark:false*/
 /*global angular,alert*/
 angular.module('notesApp')
-	.controller('notesCtrl', ['$scope', '$http', 'baseUrl', '$rootScope', '$state', function ($scope, $http, baseUrl, $rootScope, $state) {
+	.controller('notesCtrl', ['Auth','$scope', '$http', 'baseUrl', '$rootScope', '$state', 
+		function(Auth, $scope, $http, baseUrl, $rootScope, $state) {
 
+		$scope.logout = function() {
+			Auth.logout().then(function(isLoggedOut) {
+				if (isLoggedOut) {
+					$state.go('login');
+				}
+			}, function(err) {
+				console.log(err);
+			});
+		};
 
 
 	}]);
