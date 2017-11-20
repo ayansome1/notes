b53676d6e6c755800a83a2807ce7b74f008e498b
ayan <ayansome1@gmail.com>
2017-11-20 18:33:30 +0530

    login page

--- client/app/components/login/login.js
+++ client/app/components/login/login.js
@@ -5,18 +5,5 @@ angular.module('notesApp')
 	.controller('loginCtrl',['$scope', '$http', 'baseUrl', '$rootScope', '$state', function($scope, $http, baseUrl, $rootScope, $state) {
 
 		$scope.baseUrl = baseUrl;
-		console.log($scope.baseUrl);
-
-		// $scope.login = function(user) {
-		// 	$http.post(baseUrl + '/login', user)
-		// 		.success(function() {
-		// 			$rootScope.user = user;
-		// 			$state.go('/');
-		// 		})
-		// 		.error(function() {
-		// 			alert('Try again');
-		// 		});
-		// };
-		// $rootScope.$broadcast('loggedin', true);
 
 	}]);
